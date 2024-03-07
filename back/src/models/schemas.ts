c02e47885f44e99cfc4f7d4705018a95baafe826
david2am <dvccueter@gmail.com>
2024-03-07 17:33:14 -0500

    back:feat -> db schema enhancement

--- back/src/models/schemas.ts
+++ back/src/models/schemas.ts
@@ -1,12 +1,28 @@
 import * as v from 'valibot'
 
-export const EmailSchema = v.string([v.email()])
+export const EmailSchema = v.string([
+  v.minLength(1, 'Please enter your email'),
+  v.email()
+])
 export const NewUserSchema = v.object({
-  id: v.string(),
+  id: v.string([
+    v.minLength(1, 'Please enter your id'),
+    v.minLength(3, 'Please enter your '),
+  ]),
   email: EmailSchema,
-  username: v.string(),
-  firstname: v.optional(v.string()),
-  lastname: v.optional(v.string()),
-  password: v.string()
+  username: v.string([
+    v.minLength(1, 'Please enter your username'),
+    v.minLength(3, 'Your username must have 3 characters at least'),
+  ]),
+  firstname: v.optional(v.string([
+    v.minLength(3, 'Your firstname must have 3 characters at least'),
+  ])),
+  lastname: v.optional(v.string([
+    v.minLength(3, 'Your lastname must have 3 characters at least'),
+  ])),
+  password: v.string([
+    v.minLength(1, 'Please enter your password'),
+    v.minLength(8, 'Your password must have 3 characters at least'),
+  ])
 })
 export const UpdateUserSchema = v.partial(NewUserSchema)
