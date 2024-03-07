d032b951ab74804e3775c1ee51f183ed12b51efb
david2am <dvccueter@gmail.com>
2024-03-07 17:16:24 -0500

    back:feat -> remove static files endpoints

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,32 +1,13 @@
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/bun'
+import { cors } from 'hono/cors'
 
 import users from './handlers/users'
-import { Message } from "./models/types"
+import { Message } from './models/types'
 
 const app = new Hono()
 
-// Assets
-app.get('/icons/*',
-  serveStatic({
-    root: './',
-    rewriteRequestPath: (path) => path.replace(/^\/icons/, 'assets/icons/')
-}))
-app.get('/css/*',
-  serveStatic({
-    root: './',
-    rewriteRequestPath: (path) => path.replace(/^\/css/, 'assets/css/')
-}))
-
-// Pages
-app.get('/',
-  serveStatic({ path: './assets/home.html' })
-)
-app.get('/join',
-  serveStatic({ path: './assets/join.html' })
-)
-
 // APIs
+app.use('/api/*', cors({ origin: process.env.FRONT_URL! }))
 app.route('/api/users', users)
 
 // Error handling
@@ -39,8 +20,6 @@ app.onError((err, c) => {
     data: undefined
   } satisfies Message, 500 )
 })
-app.notFound((c) =>
-  c.text('The resource doesn\'t exist', 404)
-)
+app.notFound((c) => c.text('The resource doesn\'t exist', 404))
 
 export default app
